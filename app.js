f9d6e7bbe03b7e46337d18dcad7a7926520a31c5
Joshua Abu <joshujah007@gmail.com>
2016-06-26 16:42:17 -0400

    configured files for the project

---
+++ app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Interface from './Interface.js';
+
+
+
+ReactDOM.render(
+    <Interface />,
+    document.querySelector('.root')
+);
